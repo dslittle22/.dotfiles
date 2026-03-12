cfc17042c3c645a8a965c859324354b1cbf57645
Danny Little <dalittle@hubspot.com>
2026-03-12 12:13:15 -0400

    add lazydev and lua_ls settings

---
+++ config/nvim/lsp/lua_ls.lua
@@ -0,0 +1,21 @@
+-- https://www.reddit.com/r/neovim/comments/1qfidjn/comment/o16u94p/
+-- needed this to be abgle to set pathStrict = false, which per the above comment,
+-- makes lazydev work correctly. And it needs to be in an lsp/ dir to be picked up.
+return {
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+        pathStrict = false,
+      },
+      workspace = {
+        checkThirdParty = false,
+        ignoreDir = {},
+        library = { vim.env.VIMRUNTIME .. "/lua" },
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -8,6 +8,16 @@ end
 
 return {
   {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+
+  {
     "mason-org/mason.nvim",
     opts = {},
   },
